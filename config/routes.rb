e15c85676b5031ea37bf168408d0dab54e8ffdec
alegalliard <ale.sz.design@gmail.com>
2018-03-25 01:52:04 +0000

    test change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json },
-                              constraints: { subdomain: 'api' },
-                              path: '/' do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
 
   end
 end
