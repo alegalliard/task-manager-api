0944aac750d0baa519859c5e3d3b879323698ca7
alegalliard <ale.sz.design@gmail.com>
2018-04-16 02:10:42 +0000

    devise user tests with shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build(:user) }
+  let(:user) {build(:user)}
 
-  it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
+  it {expect(user).to respond_to(:email)}
+
+  #sem shoulda-matchers
+  # context 'when name is blank' do
+  #   before (:each) { user.name = " " }
+  #
+  #   it { expect(user).not_to be_valid }
+  # end
+  #
+  # context 'when name is nil' do
+  #   before { user.name = nil }
+  #   it { expect(user).not_to be_valid}
+  # end
+
+  #fazendo tudo com o shoulda-matchers
+  it { expect(user).to validate_presence_of(:name) }
+  #ou
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value("xyz@xpt.com").for(:email) }
 end
